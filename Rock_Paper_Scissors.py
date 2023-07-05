8c74c394d2e9613b32d5d6bd1bee85296616dae9
Ella <demmanuella45@gmail.com>
2023-07-05 10:55:49 +0100

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -3,67 +3,60 @@ import tkinter as tk
 
 window = tk.Tk()
 window.geometry("400x300")
-window.title("Rock paper Scissors Game")
+window.title("Rock Paper Scissors Game")
 
-User_score = 0
-Comp_score = 0
-User_choice = ""
-Comp_choice = ""
+USER_SCORE = 0
+COMP_SCORE = 0
+USER_CHOICE = ""
+COMP_CHOICE = "" 
 
-def Choice_to_number(choice):
-    reps = {"rock":0,"paper":1,"scissors":2}
-    return reps[choice]
-
-def Number_to_choice(number):
-    reps = {0:"rock", 1:"paper", 2:"scissors"}
-    return reps[number]
-
-def Computer_Choice():
-    Comp_choice = random.choice(["rock","paper","scissors"])
-    return Comp_choice
-
-def result(User_choice,Comp_choice):
-    global User_score
-    global Comp_score
-    if User_choice == Comp_choice:
-        print("It's a tie")
-    elif Choice_to_number(User_choice) < Choice_to_number(Computer_Choice):
-        User_score += 1
-        print("Comp lose")
-    else:
-        Comp_score += 1
+def choice_to_number(choice):
+    rps = {'rock':0,'paper':1,'scissor':2}
+    return rps[choice]
+def number_to_choice(number):
+    rps={0:'rock',1:'paper',2:'scissor'}
+    return rps[number]
+def random_computer_choice():
+    return random.choice(['rock','paper','scissor']) 
+def result(human_choice,comp_choice):
+    global USER_SCORE
+    global COMP_SCORE
+    user=choice_to_number(human_choice)
+    comp=choice_to_number(comp_choice)
+    if(user==comp):
+        print("Tie")
+    elif((user-comp)%3==1):
         print("You win")
-
+        USER_SCORE+=1
+    else:
+        print("Comp wins")
+        COMP_SCORE+=1
     text_area = tk.Text(master=window,height=12,width=30,bg="#FFFF99")
     text_area.grid(column=0,row=4)
-    text_area.insert(tk.END,f"User score: {User_score}\nComp score: {Comp_score}")
-
+    answer = "Your Choice: {uc} \nComputer's Choice : {cc} \n Your Score : {u} \n Computer Score : {c} ".format(uc=USER_CHOICE,cc=COMP_CHOICE,u=USER_SCORE,c=COMP_SCORE)    
+    text_area.insert(tk.END,answer)
 def rock():
-    global User_choice
-    global Comp_choice
-    User_choice = "rock"
-    Comp_choice = Computer_Choice()
-    result(User_choice,Comp_choice)
-
+    global USER_CHOICE
+    global COMP_CHOICE
+    USER_CHOICE='rock'
+    COMP_CHOICE=random_computer_choice()
+    result(USER_CHOICE,COMP_CHOICE)
 def paper():
-    global User_choice
-    global Comp_choice
-    User_choice = "paper"
-    Comp_choice = Computer_Choice()
-    result(User_choice,Comp_choice)
-
-def scissors():
-    global User_choice
-    global Comp_choice
-    User_choice = "scissors"
-    Comp_choice = Computer_Choice()
-    result(User_choice,Comp_choice)
-
-button1 = tk.Button(master=window,text="rock",command=rock, bg="skyblue")
-button1.grid(column=0,row=0)
-button2 = tk.Button(master=window,text="paper",command=paper, bg="pink")
-button2.grid(column=0, row=1)
-button3 = tk.Button(master= window,text="scissors", command= scissors)
-button3.grid(column=0,row=2)
-
+    global USER_CHOICE
+    global COMP_CHOICE
+    USER_CHOICE='paper'
+    COMP_CHOICE=random_computer_choice()
+    result(USER_CHOICE,COMP_CHOICE)
+def scissor():
+    global USER_CHOICE
+    global COMP_CHOICE
+    USER_CHOICE='scissor'
+    COMP_CHOICE=random_computer_choice() 
+    result(USER_CHOICE,COMP_CHOICE)
+button1 = tk.Button(text="       Rock       ",bg="skyblue",command=rock)
+button1.grid(column=0,row=1)
+button2 = tk.Button(text="       Paper      ",bg="pink",command=paper)
+button2.grid(column=0,row=2)
+button3 = tk.Button(text="      Scissor     ",bg="lightgreen",command=scissor)
+button3.grid(column=0,row=3)
 window.mainloop()
